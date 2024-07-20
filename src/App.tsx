c371d00f29a43c09e81ff7d1cd8f1bc97e0137ec
Muhammad Afuzarahman <theafuza@gmail.com>
2024-07-20 16:09:37 -0700

    Configure tailwind and add play button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <div>
-      <h1>Return to Olympus</h1>
+      <h1 className="mb-2 uppercase font-bold mb-20">
+        R<span className="text-4xl">eturn</span>{' '}
+        <span className="text-4xl">to</span> O
+        <span className="text-4xl">lympus</span>
+      </h1>
+
+      <button className="w-[300px] hover:border-white">Play</button>
     </div>
   );
 }
