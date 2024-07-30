36c5724df41d5693909080aee97ce249fde3a7c9
Muhammad Afuzarahman <theafuza@gmail.com>
2024-07-30 04:59:06 -0700

    Add initial DialogOverlay component in game page

---
+++ src/overlays/DialogOverlay.tsx
@@ -0,0 +1,10 @@
+export const DialogOverlay = () => {
+  return (
+    <div className="fixed z-50 bottom-0 bg-black w-full min-h-[200px] py-5 px-10 shadow-sm shadow-white">
+      <h2 className="text-white">Dialog Overlay</h2>
+      <p className="text-white">
+        This is the dialog overlay. It is used to display a dialog.
+      </p>
+    </div>
+  );
+};
