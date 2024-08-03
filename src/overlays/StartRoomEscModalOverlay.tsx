46cfab2155beee3c49fca929499c52d005b867ac
Muhammad Afuzarahman <theafuza@gmail.com>
2024-08-03 15:48:23 -0700

    Conditionally render and close StartRoom escape modal

--- src/overlays/StartRoomEscModalOverlay.tsx
+++ src/overlays/StartRoomEscModalOverlay.tsx
@@ -1,19 +1,32 @@
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../components/Button';
+import { FC } from 'react';
 
-export const StartRoomEscModalOverlay = () => {
+export const StartRoomEscModalOverlay: FC<{ onClose: () => void }> = ({
+  onClose,
+}) => {
   const navigate = useNavigate();
 
   return (
     <div className="fixed w-full h-full z-[100] flex justify-center items-center">
-      <div className="absolute top-0 left-0 w-screen h-screen bg-black opacity-10"></div>
-      <div className="z-10 bg-neutral-900 p-10">
+      <div
+        className="absolute top-0 left-0 w-screen h-screen bg-black opacity-10"
+        onClick={onClose}
+      ></div>
+      <div className="z-10 bg-neutral-900 p-10 flex flex-col">
         <Button
           onClick={() => {
             navigate('/');
           }}
         >
           Back to Main Menu
+        </Button>
+        <Button
+          onClick={() => {
+            onClose();
+          }}
+        >
+          Back to Game
         </Button>
       </div>
     </div>
--- src/pages/Game/StartRoom.tsx
+++ src/pages/Game/StartRoom.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Button } from '../../components/Button';
 import { SCRIPT_SHOP, SCRIPT_SHOP_TUTORIAL } from '../../constants/scripts';
 import { StartRoomEscModalOverlay } from '../../overlays/StartRoomEscModalOverlay';
@@ -8,11 +9,30 @@ export const StartRoom = () => {
   const { setCurrentScript } = useScriptStore();
   const { selectedCampaign } = useCampaignStore();
 
+  const [escModalOpen, setEscModalOpen] = useState(false);
+
+  // Assign event listerners to the window object
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        setEscModalOpen(!escModalOpen);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [escModalOpen]);
+
   if (!selectedCampaign) return null;
 
   return (
     <>
-      <StartRoomEscModalOverlay />
+      {escModalOpen && (
+        <StartRoomEscModalOverlay onClose={() => setEscModalOpen(false)} />
+      )}
       <h1>Start Room</h1>
       <p>Will be a small room where player starts a run</p>
       <Button
