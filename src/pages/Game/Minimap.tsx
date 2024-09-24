537b18d1af1ab46a5aa628ef0a09715bde9fb92a
Muhammad Afuzarahman <theafuza@gmail.com>
2024-09-24 15:42:08 -0700

    Implement door indicators in minimap

--- src/pages/Game/Minimap.tsx
+++ src/pages/Game/Minimap.tsx
@@ -26,19 +26,60 @@ export const Minimap: FC = () => {
   );
 };
 
+const DOOR_BG_CLASSNAME = 'bg-white';
+
 const RoomNode: FC<{ room: IRoom; active: boolean }> = ({ room, active }) => {
   return (
     <div className={`w-8 h-8 p-1 bg-zinc-900`}>
       <div
-        className={clsx('w-full h-full flex justify-center items-center p-2', {
-          'bg-none': !room.isKnown,
-          'bg-green-500': room.isKnown && room.type === ROOM_TYPE.START,
-          'bg-red-500': room.isKnown && room.type === ROOM_TYPE.BOSS,
-          'bg-blue-500': room.isKnown && room.type === ROOM_TYPE.COMMON,
-        })}
+        className={clsx(
+          'relative w-full h-full flex justify-center items-center p-2',
+          {
+            'bg-none': !room.isKnown,
+            'bg-green-500': room.isKnown && room.type === ROOM_TYPE.START,
+            'bg-red-500': room.isKnown && room.type === ROOM_TYPE.BOSS,
+            'bg-blue-500': room.isKnown && room.type === ROOM_TYPE.COMMON,
+          }
+        )}
       >
         {active && (
           <div className={clsx('bg-black rounded-full w-2 h-2')}></div>
+        )}
+        {room.isCleared && room.northDoor && (
+          <div
+            className={clsx(
+              'absolute top-0 left-[50%] w-2 h-1',
+              DOOR_BG_CLASSNAME
+            )}
+            style={{ transform: 'translate(-50%, -50%)' }}
+          ></div>
+        )}
+        {room.isCleared && room.southDoor && (
+          <div
+            className={clsx(
+              'absolute bottom-0 left-[50%] w-2 h-1',
+              DOOR_BG_CLASSNAME
+            )}
+            style={{ transform: 'translate(-50%, 50%)' }}
+          ></div>
+        )}
+        {room.isCleared && room.eastDoor && (
+          <div
+            className={clsx(
+              'absolute top-[50%] right-0 w-1 h-2',
+              DOOR_BG_CLASSNAME
+            )}
+            style={{ transform: 'translate(50%, -50%)' }}
+          ></div>
+        )}
+        {room.isCleared && room.westDoor && (
+          <div
+            className={clsx(
+              'absolute top-[50%] left-0 w-1 h-2',
+              DOOR_BG_CLASSNAME
+            )}
+            style={{ transform: 'translate(-50%, -50%)' }}
+          ></div>
         )}
       </div>
     </div>
